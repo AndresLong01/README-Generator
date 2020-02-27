836bf64ce06d1a3570aa6eae7acc0e50e2e8e223
Andres Long <andreslong95@gmail.com>
2020-02-27 17:17:16 -0600

    Markdown formatting v 0.5

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,38 @@
 function generateMarkdown(data) {
   return `
-# ${data.title}
+  # ${data.title}
+  [![Generic Badge](https://img.shields.io/badge/User-Andres%20Long-red.svg)](https://github.com/${data.username})
 
-`;
+  # Description
+  ${data.description}
+
+  Table of Contents |
+  ----------------- |
+  Instalation Information |
+  Usage Information |
+  License |
+  Contributors |
+  Questions |
+
+  # Installation
+  ${data.install}
+
+  # Usage
+  ${data.usage}
+
+  # License
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+  # Contributors
+  ${data.contributions}
+
+  # Questions
+  If you have any questions please contact me at at: ${data.email ? data.email : "e-mail address is protected"}
+
+  My user is also linked at the top under the Generic Badge
+
+  ![profile picture](${data.image} "Profile Picture")
+  `;
 }
 
 module.exports = generateMarkdown;
